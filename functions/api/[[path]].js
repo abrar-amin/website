5fd6c0d7036212604f1ebad9e9f8e2e43187920f
Abrar Amin <abraramin.school@gmail.com>
2025-11-29 21:16:36 -0500

    http issue

--- functions/api/[[path]].js
+++ functions/api/[[path]].js
@@ -1,22 +1,40 @@
 export async function onRequest(context) {
   const url = new URL(context.request.url);
-  const path = context.params.path.join('/');
 
-  // Proxy to your backend server
-  const backendUrl = `http://34.172.84.37:5000/api/${path}`;
+  // Extract the path after /api/
+  const path = context.params.path ? context.params.path.join('/') : '';
 
-  const response = await fetch(backendUrl, {
-    method: context.request.method,
-    headers: context.request.headers,
-    body: context.request.body,
-  });
+  const backendUrl = `http://34.172.84.37:5000/api/${path}${url.search}`;
 
-  return new Response(response.body, {
-    status: response.status,
-    statusText: response.statusText,
-    headers: {
-      ...Object.fromEntries(response.headers),
-      'Access-Control-Allow-Origin': '*',
-    },
-  });
+  try {
+    const response = await fetch(backendUrl, {
+      method: context.request.method,
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: context.request.method !== 'GET' && context.request.method !== 'HEAD'
+        ? await context.request.text()
+        : undefined,
+    });
+
+    const data = await response.text();
+
+    return new Response(data, {
+      status: response.status,
+      statusText: response.statusText,
+      headers: {
+        'Content-Type': 'application/json',
+        'Access-Control-Allow-Origin': '*',
+        'Access-Control-Allow-Methods': 'GET, POST, OPTIONS',
+        'Access-Control-Allow-Headers': 'Content-Type',
+      },
+    });
+  } catch (error) {
+    return new Response(JSON.stringify({ error: error.message }), {
+      status: 500,
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    });
+  }
 }
